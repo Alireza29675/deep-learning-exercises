30799484451bf8a6f85a1d625f2b6c1e8e8cf831
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-14 13:52:56 +0330

    Perceptron has initial random weights with training power

--- exercises/0.Perseptrons/Perceptron.js
+++ exercises/0.Perseptrons/Perceptron.js
@@ -1,13 +1,23 @@
 const activation = sum => Math.sign(sum) || 1;
+const rand = (a, b) => Math.random() * (b - a) + a;
 
 class Perceptron {
     constructor () {
-        this.weights = [0.5, 0.3];
+        this.weights = [rand(-1, 1), rand(-1, 1)];
     }
     guess (inputs) {
         let sum = 0;
         for (let i in this.weights) sum += inputs[i] * this.weights[i];
         return activation(sum)
+    }
+    train (inputs, target) {
+        const guess = this.guess(inputs);
+        const error = target - guess;
+
+        // Tune all the weights
+        for (let i in this.weights) {
+            this.weights[i] += error * inputs[i];
+        }
     }
 }
 
