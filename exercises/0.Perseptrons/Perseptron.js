475d59c336326b10c22ea2eb72fecc92be06c375
Alireza Sheikholmolouki <alireza.sheikholmolouki@gmail.com>
2017-10-14 13:45:25 +0330

    first Perseptron

---
+++ exercises/0.Perseptrons/Perseptron.js
@@ -0,0 +1,14 @@
+const activation = sum => Math.sign(sum) || 1;
+
+class Perseptron {
+    constructor () {
+        this.weights = [0.5, 0.3];
+    }
+    guess (inputs) {
+        let sum = 0;
+        for (let i in this.weights) sum += inputs[i] * this.weights[i];
+        return activation(sum)
+    }
+}
+
+module.exports = Perseptron;
---
+++ exercises/0.Perseptrons/index.js
@@ -0,0 +1,4 @@
+const Perseptron = require('./Perseptron');
+
+const p = new Perseptron();
+console.log(p.guess([-0.3, 0.5]));
